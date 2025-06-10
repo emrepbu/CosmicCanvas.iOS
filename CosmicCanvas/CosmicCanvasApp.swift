d400d2682ba27b54a861c46ee3015ee3809f9ddf
emrepbu <emrewindowsarg@gmail.com>
2025-06-10 19:53:15 +0300

    Initial Commit

---
+++ CosmicCanvas/CosmicCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosmicCanvasApp.swift
+//  CosmicCanvas
+//
+//  Created by emre argana on 10.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct CosmicCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
